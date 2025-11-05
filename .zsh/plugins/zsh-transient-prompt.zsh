05563aae43545347f6ba4a97ef782dbed28e7b8f
felipe cavalcante miranda <fecmbr@gmail.com>
2025-11-04 23:26:31 -0300

    fix: Correct source path for zsh-transient-prompt plugin

--- .zsh/plugins/zsh-transient-prompt.zsh
+++ .zsh/plugins/zsh-transient-prompt.zsh
@@ -1,6 +1,6 @@
 # Source zsh-transient-prompt plugin
-if [ -f "${HOME}/.zsh-plugins/zsh-transient-prompt/zsh-transient-prompt.zsh" ]; then
-    source "${HOME}/.zsh-plugins/zsh-transient-prompt/zsh-transient-prompt.zsh"
+if [ -f "${HOME}/.zsh-plugins/zsh-transient-prompt/transient-prompt.plugin.zsh" ]; then
+    source "${HOME}/.zsh-plugins/zsh-transient-prompt/transient-prompt.plugin.zsh"
 
     TRANSIENT_PROMPT_TRANSIENT_PROMPT='% '
 fi
