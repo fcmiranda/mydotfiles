8434555c5093e34f2b086b4b1e288acb89340916
felipe cavalcante miranda <fecmbr@gmail.com>
2025-11-05 22:36:59 -0300

    feat: Add vi command mode key bindings in binds.zsh and source it in omc.sh

---
+++ .zsh/binds.zsh
@@ -0,0 +1,3 @@
+# binds to switch to vi command mode with ESC key
+bindkey -e
+bindkey '\e' vi-cmd-mode
--- .zsh/omc.sh
+++ .zsh/omc.sh
@@ -1,6 +1,9 @@
+ZSH="${ZSH_PLUGINS:-${HOME}/.zsh}"
 ZSH_MODULE_DIR="${ZSH_MODULE_DIR:-${HOME}/.zsh/modules}"
 ZSH_PLUGINS="${ZSH_PLUGINS:-${HOME}/.zsh/plugins}"
 
+# shellcheck source=/dev/null
+source "${ZSH}/binds.zsh"
 # shellcheck source=/dev/null
 source "${ZSH_MODULE_DIR}/mise.zsh"
 # shellcheck source=/dev/null
