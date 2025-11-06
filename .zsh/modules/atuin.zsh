729ec38c6fc6b6cf573beea98e4bb7a6c9408284
felipe cavalcante miranda <fecmbr@gmail.com>
2025-11-06 06:31:10 -0300

    feat: Add incremental history search with arrow keys in atuin.zsh

--- .zsh/modules/atuin.zsh
+++ .zsh/modules/atuin.zsh
@@ -3,3 +3,7 @@ if command -v atuin &> /dev/null; then
   eval "$(atuin init zsh)"
   bindkey "^R" atuin-search
 fi
+
+# incremental history search with arrow keys
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward]]
